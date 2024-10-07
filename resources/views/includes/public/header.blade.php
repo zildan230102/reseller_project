2113375e64e869231a3735c608057113a18a2429
zildan230102 <[muhammadzildan230102@gmail.com]>
2024-10-07 11:02:13 +0700

    menambahkan halaman controller, view dan routes profil

--- resources/views/includes/public/header.blade.php
+++ resources/views/includes/public/header.blade.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
 </head>
+
 <body>
-    <div class="header" id=".header">
+<header>
+    <div class="header" id="header">
         <nav class="navbar navbar-expand navbar-light">
             <img src="{{ ('style/src/img/icons/d2.png') }}" class="img-fluid" alt="Deepublish Logo">
+
             <div class="navbar-collapse collapse">
                 <ul class="navbar-nav navbar-align">
                     <li class="nav-item dropdown">
@@ -19,7 +24,8 @@
                                 <span class="indicator">4</span>
                             </div>
                         </a>
-                        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end py-0" aria-labelledby="alertsDropdown">
+                        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end py-0"
+                            aria-labelledby="alertsDropdown">
                             <div class="dropdown-menu-header">
                                 4 New Notifications
                             </div>
@@ -31,7 +37,8 @@
                                         </div>
                                         <div class="col-10">
                                             <div class="text-dark">Update completed</div>
-                                            <div class="text-muted small mt-1">Restart server 12 to complete the update.</div>
+                                            <div class="text-muted small mt-1">Restart server 12 to complete the update.
+                                            </div>
                                             <div class="text-muted small mt-1">30m ago</div>
                                         </div>
                                     </div>
@@ -43,7 +50,8 @@
                                         </div>
                                         <div class="col-10">
                                             <div class="text-dark">Lorem ipsum</div>
-                                            <div class="text-muted small mt-1">Aliquam ex eros, imperdiet vulputate hendrerit et.</div>
+                                            <div class="text-muted small mt-1">Aliquam ex eros, imperdiet vulputate
+                                                hendrerit et.</div>
                                             <div class="text-muted small mt-1">2h ago</div>
                                         </div>
                                     </div>
@@ -77,21 +85,26 @@
                             </div>
                         </div>
                     </li>
-                    
+
                     <li class="nav-item dropdown">
                         <a class="nav-icon dropdown-toggle d-inline-block d-sm-none" href="#" data-bs-toggle="dropdown">
                             <i class="align-middle" data-feather="settings"></i>
                         </a>
                         <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
-                            <img src="{{ ('style/src/img/avatars/avatar.jpg') }}" class="avatar img-fluid rounded me-1" alt="Charles Hall" />
+                            <img src="{{ ('style/src/img/avatars/avatar.jpg') }}" class="avatar img-fluid rounded me-1"
+                                alt="Charles Hall" />
                             <span class="text-dark">Charles Hall</span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end">
-                            <a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
+                            <a class="dropdown-item" href="{{ url('/profil') }}"><i class="align-middle me-1" data-feather="user"></i>
+                                Profile</a>
                             <a class="dropdown-item text-danger" href="#">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"/>
-                                    <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z" />
+                                    <path fill-rule="evenodd"
+                                        d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z" />
                                 </svg>
                                 <span class="align-middle me-1"></span> Logout
                             </a>
@@ -101,4 +114,7 @@
             </div>
         </nav>
     </div>
-</div>
+    </div>
+    </header>
+</body>
+</html>
