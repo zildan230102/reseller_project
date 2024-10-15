02ee391ea32838ef4e0bcb46bd102ee0cf6b80d0
retnoristiani <retnoristiani@students.amikom.ac.id>
2024-10-15 09:20:12 +0700

    feat: navbar

--- resources/views/includes/public/header.blade.php
+++ resources/views/includes/public/header.blade.php
@@ -51,115 +51,116 @@
                                 </ul>
                             </li>
                         </ul>
-                    </header>
-                </div>
 
-                <div class="navbar-collapse collapse">
-                    <ul class="navbar-nav navbar-align">
-                        <li class="nav-item dropdown">
-                            <a class="nav-icon dropdown-toggle" href="#" id="alertsDropdown" data-bs-toggle="dropdown">
-                                <div class="position-relative">
-                                    <i class="align-middle" data-feather="bell"></i>
-                                    <span class="indicator">4</span>
-                                </div>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end py-0"
-                                aria-labelledby="alertsDropdown">
-                                <div class="dropdown-menu-header">
-                                    4 New Notifications
-                                </div>
-                                <div class="list-group">
-                                    <a href="#" class="list-group-item">
-                                        <div class="row g-0 align-items-center">
-                                            <div class="col-2">
-                                                <i class="text-danger" data-feather="alert-circle"></i>
-                                            </div>
-                                            <div class="col-10">
-                                                <div class="text-dark">Update completed</div>
-                                                <div class="text-muted small mt-1">Restart server 12 to complete the
-                                                    update.</div>
-                                                <div class="text-muted small mt-1">30m ago</div>
-                                            </div>
+                        <div class="navbar-collapse collapse">
+                            <ul class="navbar-nav navbar-align">
+                                <li class="nav-item dropdown">
+                                    <a class="nav-icon dropdown-toggle" href="#" id="alertsDropdown" data-bs-toggle="dropdown">
+                                        <div class="position-relative">
+                                            <i class="align-middle" data-feather="bell"></i>
+                                            <span class="indicator">4</span>
                                         </div>
                                     </a>
-                                    <a href="#" class="list-group-item">
-                                        <div class="row g-0 align-items-center">
-                                            <div class="col-2">
-                                                <i class="text-warning" data-feather="bell"></i>
-                                            </div>
-                                            <div class="col-10">
-                                                <div class="text-dark">Update your product stock information before
-                                                    October 31</div>
-                                                <div class="text-muted small mt-1">Update the stock immediately.</div>
-                                                <div class="text-muted small mt-1">2h ago</div>
-                                            </div>
+                                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end py-0"
+                                        aria-labelledby="alertsDropdown">
+                                        <div class="dropdown-menu-header">
+                                            4 New Notifications
                                         </div>
-                                    </a>
-                                    <a href="#" class="list-group-item">
-                                        <div class="row g-0 align-items-center">
-                                            <div class="col-2">
-                                                <i class="text-primary" data-feather="home"></i>
-                                            </div>
-                                            <div class="col-10">
-                                                <div class="text-dark">Login from 192.186.1.8</div>
-                                                <div class="text-muted small mt-1">5h ago</div>
-                                            </div>
-                                        </div>
-                                    </a>
-                                    <a href="#" class="list-group-item">
-                                        <div class="row g-0 align-items-center">
-                                            <div class="col-2">
-                                                <i class="text-success" data-feather="user-plus"></i>
-                                            </div>
-                                            <div class="col-10">
-                                                <div class="text-dark">New connection</div>
-                                                <div class="text-muted small mt-1">Christina accepted your request.
+                                        <div class="list-group">
+                                            <a href="#" class="list-group-item">
+                                                <div class="row g-0 align-items-center">
+                                                    <div class="col-2">
+                                                        <i class="text-danger" data-feather="alert-circle"></i>
+                                                    </div>
+                                                    <div class="col-10">
+                                                        <div class="text-dark">Update completed</div>
+                                                        <div class="text-muted small mt-1">Restart server 12 to complete the
+                                                            update.</div>
+                                                        <div class="text-muted small mt-1">30m ago</div>
+                                                    </div>
                                                 </div>
-                                                <div class="text-muted small mt-1">14h ago</div>
+                                            </a>
+                                            <a href="#" class="list-group-item">
+                                                <div class="row g-0 align-items-center">
+                                                    <div class="col-2">
+                                                        <i class="text-warning" data-feather="bell"></i>
+                                                    </div>
+                                                    <div class="col-10">
+                                                        <div class="text-dark">Update your product stock information before
+                                                            October 31</div>
+                                                        <div class="text-muted small mt-1">Update the stock immediately.</div>
+                                                        <div class="text-muted small mt-1">2h ago</div>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <a href="#" class="list-group-item">
+                                                <div class="row g-0 align-items-center">
+                                                    <div class="col-2">
+                                                        <i class="text-primary" data-feather="home"></i>
+                                                    </div>
+                                                    <div class="col-10">
+                                                        <div class="text-dark">Login from 192.186.1.8</div>
+                                                        <div class="text-muted small mt-1">5h ago</div>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <a href="#" class="list-group-item">
+                                                <div class="row g-0 align-items-center">
+                                                    <div class="col-2">
+                                                        <i class="text-success" data-feather="user-plus"></i>
+                                                    </div>
+                                                    <div class="col-10">
+                                                        <div class="text-dark">New connection</div>
+                                                        <div class="text-muted small mt-1">Christina accepted your request.
+                                                        </div>
+                                                        <div class="text-muted small mt-1">14h ago</div>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <div class="dropdown-menu-footer">
+                                                <a href="#" class="text-muted">Show all notifications</a>
                                             </div>
                                         </div>
-                                    </a>
-                                    <div class="dropdown-menu-footer">
-                                        <a href="#" class="text-muted">Show all notifications</a>
                                     </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-icon dropdown-toggle d-inline-block d-sm-none" href="#"
-                                data-bs-toggle="dropdown">
-                                <i class="align-middle" data-feather="settings"></i>
-                            </a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-icon dropdown-toggle d-inline-block d-sm-none" href="#"
+                                        data-bs-toggle="dropdown">
+                                        <i class="align-middle" data-feather="settings"></i>
+                                    </a>
 
-                            <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#"
-                                data-bs-toggle="dropdown">
-                                <img src="{{ ('style/src/img/avatars/avatar.jpg') }}"
-                                    class="avatar img-fluid rounded me-1" alt="Charles Hall" />
-                                <span class="text-dark">{{ Auth::user()->name }}</span>
-                            </a>
+                                    <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#"
+                                        data-bs-toggle="dropdown">
+                                        <img src="{{ ('style/src/img/avatars/avatar.jpg') }}"
+                                            class="avatar img-fluid rounded me-1" alt="Charles Hall" />
+                                        <span class="text-dark">{{ Auth::user()->name }}</span>
+                                    </a>
 
-                            <div class="dropdown-menu dropdown-menu-end">
-                                <a class="dropdown-item" href="{{ url('profil') }}"><i class="align-middle me-1"
-                                        data-feather="user"></i>
-                                    Profile</a>
-
-                                <form action="{{ route('logout') }}" method="POST" style="display: none;"
-                                    id="logout-form">
-                                    @csrf
-                                </form>
-                                <a class="dropdown-item" href="#"
-                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                        class="bi bi-box-arrow-left" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd"
-                                            d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z" />
-                                        <path fill-rule="evenodd"
-                                            d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z" />
-                                    </svg> Logout</a>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
+                                    <div class="dropdown-menu dropdown-menu-end">
+                                        <a class="dropdown-item" href="{{ url('profil') }}"><i class="align-middle me-1"
+                                                data-feather="user"></i>
+                                            Profile
+                                        </a>
+                                        <form action="{{ route('logout') }}" method="POST" style="display: none;"
+                                            id="logout-form">
+                                            @csrf
+                                        </form>
+                                        <a class="dropdown-item" href="#"
+                                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                                class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+                                                <path fill-rule="evenodd"
+                                                    d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z" />
+                                                <path fill-rule="evenodd"
+                                                    d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z" />
+                                            </svg> Logout
+                                        </a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </header>
+                </div> 
             </nav>
         </div>
     </header>
