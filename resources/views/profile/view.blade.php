d89059c82a89a63e9f075dab2f7955e5a3f33518
lutfianaade <155591865+lutfianaade@users.noreply.github.com>
2024-10-16 15:41:21 +0700

    feat : profil

--- resources/views/profile/view.blade.php
+++ resources/views/profile/view.blade.php
@@ -1,32 +1,52 @@
 @extends('layouts.profil')
 @section('title', 'Profil')
 @section('content')
-<div class="container">
+
+
+<div class="container mt-3">
     <h2>Profil Pengguna</h2>
 
     @if(session('success'))
     <div class="alert alert-success">{{ session('success') }}</div>
     @endif
 
-    <div class="card mb-4">
-        <div class="card-body text-center">
+    <div class="card mb-4 mt-3">
+        <div class="card-title text-center">
             @if ($user->profile_picture)
             <img src="{{ asset('storage/' . $user->profile_picture) }}?{{ time() }}" alt="Foto Profil"
-                class="rounded-circle" width="150" height="150">
+                class="rounded-circle mt-5" width="150" height="150">
             @else
             <img src="https://via.placeholder.com/150" alt="Foto Profil" class="rounded-circle" width="150"
                 height="150">
             @endif
 
-            <h4 class="mt-3">{{ $user->name }}</h4>
-            <p><strong>Email:</strong> {{ $user->email }}</p>
-            <p><strong>No Telepon:</strong> {{ $user->phone }}</p>
-            <p><strong>Alamat:</strong> {{ $user->address }}</p>
-            <p><strong>Tanggal Bergabung:</strong>
-                {{ $user->join_date ? \Carbon\Carbon::parse($user->join_date)->format('d M Y') : 'Belum ada' }}</p>
-
-            <a href="{{ route('profile.edit') }}" class="btn btn-primary">Edit Profil</a>
+            <h4 class="mt-5">{{ $user->name }}</h4>
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered">
+                <tbody>
+                    <tr>
+                        <th>Email</th>
+                        <td>{{ $user->email }}</td>
+                    </tr>
+                    <tr>
+                        <th>No Telepon</th>
+                        <td>{{ $user->phone }}</td>
+                    </tr>
+                    <tr>
+                        <th>Alamat</th>
+                        <td>{{ $user->address }}</td>
+                    </tr>
+                    <tr>
+                        <th>Tanggal bergabung</th>
+                        <td>{{ $user->join_date ? \Carbon\Carbon::parse($user->join_date)->format('d M Y') : 'Belum ada' }}</td>
+                    </tr>
+                </tbody>
+            </table>
+                <a href="{{ route('profile.edit') }}" class="btn btn text-orange">Edit Profil</a>
+        </div>
         </div>
     </div>
+    
 </div>
 @endsection
