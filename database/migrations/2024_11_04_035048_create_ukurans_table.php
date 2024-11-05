77a13f9d500e87312d9eeef77715e330c8a8aa1b
zildan230102 <[muhammadzildan230102@gmail.com]>
2024-11-05 11:08:37 +0700

    feat:migrasi

--- database/migrations/2024_11_04_035048_create_ukurans_table.php
+++ database/migrations/2024_11_04_035048_create_ukurans_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUkuranTable extends Migration
+class CreateUkuransTable extends Migration
 {
     public function up()
     {
