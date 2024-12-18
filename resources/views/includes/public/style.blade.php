24c7dafe1690813c5ab2764cb427329d3bf40649
lutfianaade <155591865+lutfianaade@users.noreply.github.com>
2024-12-18 15:57:48 +0700

    feat: fe

--- resources/views/includes/public/style.blade.php
+++ resources/views/includes/public/style.blade.php
@@ -21,6 +21,8 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+	<link rel="stylesheet" href="{{asset('css/footer.css')}}">
+	<link rel="stylesheet" href="{{asset('css/header.css')}}">
 
 	 {{-- // Sweet Alert  --}}
 	 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
