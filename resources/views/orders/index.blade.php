d2ab4d43bcabc8c63dde0ad00467a66e259e905d
lutfianaade <155591865+lutfianaade@users.noreply.github.com>
2025-01-03 09:24:39 +0700

    feat: daftar order

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -131,6 +131,7 @@
 
     .order-title {
         font-size: 1rem;
+    }
 
     .text-title {
         font-size: 18px;
@@ -151,8 +152,6 @@
         padding: 0.2rem 0.5rem;
     }
 
-
-    .dropdown-menu {
 
     .table .dropdown-menu {
 
